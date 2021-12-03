b18a5570b5b324c4c8febf088f1299d0453689e6
Reed Hamilton <rhamilt@uw.edu>
2021-12-02 23:24:18 -0800

    Day 3: Binary Diagnostic Minor Changes

--- 2021/3.rb
+++ 2021/3.rb
@@ -1,13 +1,14 @@
 lines = File.open("3.txt").readlines.map(&:strip)#.map(&:to_i)
 
 total = Hash.new(0)
-(0...12).each do |idx| #For the 12 bits
+bits = lines[0].length
+(0...bits).each do |idx| #For the 12 bits
 	lines.each { |line| total[idx] += line[idx].to_i }
 end
 sum = ""
 inv = ""
-total.each {|idx, num| num > 500 ? sum << "1" : sum << "0"}
-total.each {|idx, num| num < 500 ? inv << "1" : inv << "0"}
+total.each {|idx, num| num > lines.length / 2 ? sum << "1" : sum << "0"}
+total.each {|idx, num| num < lines.length / 2 ? inv << "1" : inv << "0"}
 =begin part 1
 	My roommates asked to watch and it really threw me off. I really like my solution
 	but I just didn't get it quickly. Very disappointed in myself
@@ -18,7 +19,7 @@ oxygenGenerator = ""
 co2_scrubber = ""
 oxy_lines = lines.clone
 co2_lines = lines.clone
-(0...12).each do |idx|
+(0...bits).each do |idx|
 	total = 0
 	oxy_lines.each { |oxy_line| total += oxy_line[idx].to_i	}
 	total < oxy_lines.length.to_f / 2 ? oxygenGenerator += "0" : oxygenGenerator += "1"
