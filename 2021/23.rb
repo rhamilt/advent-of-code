a20856366ee71b154ee2b87c80e7c9e7aba9e69d
Reed Hamilton <rhamilt@uw.edu>
2021-12-23 13:35:12 -0500

    Day 23: Amphipod

---
+++ 2021/23.rb
@@ -0,0 +1,6 @@
+lines = File.open("23.txt").readlines.map(&:strip)
+
+=begin part 1 and part 2
+	Actually just did them both by hand, took a while but not too hard.
+=end
+puts "I did them both by hand :)"
