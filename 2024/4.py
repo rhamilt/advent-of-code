19839d4d9da50639472f6333e15579d205bf7efe
Reed Hamilton <reedphamilton@icloud.com>
2024-12-09 01:51:22 +0000

    Day 4: Ceres Search; part 1

---
+++ 2024/4.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+lines = open("4.txt").readlines()
+grid = defaultdict(str, {(r, c): lines[r][c] for r in range(len(lines)) for c in range(len(lines[0]))})
+
+keys = list(grid.keys())
+d = -1, 0, 1
+print (sum("".join([grid[(r + i * n, c + j * n)] for n in range(4)]) == "XMAS" for r, c in keys for i in d for j in d))
+
