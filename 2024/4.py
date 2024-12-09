36811d84377e2c16e413c33ea6699cc28e41be19
Reed Hamilton <rhamilt@uw.edu>
2024-12-08 18:13:13 -0800

    Day 4: Ceres Search

--- 2024/4.py
+++ 2024/4.py
@@ -5,5 +5,8 @@ grid = defaultdict(str, {(r, c): lines[r][c] for r in range(len(lines)) for c in
 
 keys = list(grid.keys())
 d = -1, 0, 1
+
 print (sum("".join([grid[(r + i * n, c + j * n)] for n in range(4)]) == "XMAS" for r, c in keys for i in d for j in d))
 
+massam = "MAS", "SAM"
+print (sum("".join([grid[(r + i, c + i)] for i in d]) in massam and "".join([grid[(r + i, c - i)] for i in d]) in massam for r, c in keys))
